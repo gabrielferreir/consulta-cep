c3c787fab69dd8b24ff38948d5541193443a79bc
Gabriel Ferreira <gabrielferrer@outlook.com.br>
2019-10-10 22:57:53 -0300

    Init text address

--- src/views/mainPage.js
+++ src/views/mainPage.js
@@ -4,7 +4,6 @@ import {cepRequest} from "../actions/mainActions";
 import Header from "./header";
 
 
-
 class MainPage extends Component {
     render() {
         console.log(this.props);
@@ -14,9 +13,19 @@ class MainPage extends Component {
             <main className="main">
                 <Header/>
 
-                <iframe
-                    src="https://maps.google.com/maps?q=14402336&amp;z=15&amp;output=embed"
-                    width="800" height="600" frameborder="0" allowfullscreen></iframe>
+                <section style={{
+                    border: '2px solid #CCC',
+                    padding: '32px',
+                    marginTop: '8px'
+                }}>
+                    <h1>Rua Miguel Mentem</h1>
+                    <h2>Vila Guilherme</h2>
+                    <h3>São Paulo - SP</h3>
+                    <h4>02050-010</h4>
+                    <iframe
+                        src="https://maps.google.com/maps?q=14402336&amp;z=15&amp;output=embed"
+                        width="800" height="600" frameborder="0" allowfullscreen></iframe>
+                </section>
 
             </main>
         );
