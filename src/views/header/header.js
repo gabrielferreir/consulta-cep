04b5f55eb82fe748576730e5be07fd324c2dbf2b
Gabriel Ferreira <gabrielferrer@outlook.com.br>
2019-10-13 00:00:59 -0300

    fix type input

--- src/views/header/header.js
+++ src/views/header/header.js
@@ -15,7 +15,7 @@ export class Header extends React.Component {
 
                 <div className="header__wrapper-field">
                     <label className="header__label-field">CEP</label>
-                    <input type="text" placeholder="02050-010" value={cepValue}
+                    <input type="number" placeholder="02050-010" value={cepValue}
                            onChange={event => {
                                updateCep(event.target.value)
                            }}
