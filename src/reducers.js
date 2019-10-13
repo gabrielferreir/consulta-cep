634f7801fca9cb13c4d4ffef043a25b14880a06d
Gabriel Ferreira <gabrielferrer@outlook.com.br>
2019-10-13 00:08:20 -0300

    --

--- src/reducers.js
+++ src/reducers.js
@@ -2,9 +2,7 @@
 
 import {combineReducers} from 'redux';
 import mainReducer from './reducer/mainReducer';
-import {reducer as formReducer} from 'redux-form';
 
 export default combineReducers({
-    mainReducer,
-    form: formReducer
+    mainReducer
 });
